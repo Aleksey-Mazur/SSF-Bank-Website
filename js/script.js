b53a54ef366844cfb2d063fa4c8c6774a61d6793
Aleksey <1aleksey.mazur@gmail.com>
2022-08-16 01:52:12 +0300

    modal window functionality implemented; responsive modal added

--- js/script.js
+++ js/script.js
@@ -28,3 +28,28 @@ navLink.forEach(link => {
     });
   });
 });
+
+// ========== MODAL WINDOW ==========
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btnOpen => btnOpen.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
