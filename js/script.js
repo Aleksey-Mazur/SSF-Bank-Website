6fb71c20ababa1bbc801369fe94e59d118fc53f0
Aleksey <1aleksey.mazur@gmail.com>
2022-08-17 11:09:58 +0300

    fixed menu-burger icon; smooth scrolling implemented; page navigation smooth scrolling refactored

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 'use strict';
 
-// ========== MENU BURGER ==========
 const burgerIcon = document.querySelector('.menu-icon');
 const navMenu = document.querySelector('.nav');
 const navLink = document.querySelectorAll('.nav__link');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+// ========== MENU BURGER ==========
 burgerIcon.addEventListener('click', () => {
   document.body.classList.toggle('_lock');
   burgerIcon.classList.toggle('_active');
@@ -19,23 +25,9 @@ navLink.forEach(link => {
   });
 });
 
-// ========== SMOOTH SCROLL ==========
-navLink.forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth',
-    });
-  });
-});
-
 // ========== MODAL WINDOW ==========
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -45,11 +37,29 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btnOpen => btnOpen.addEventListener('click', openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
+
+// ========== SMOOTH SCROLL ==========
+btnScrollTo.addEventListener('click', e =>
+  section1.scrollIntoView({ behavior: 'smooth' })
+);
+
+// ========== PAGE NAVIGATION ==========
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    e.target.classList.contains('nav__link') &&
+    !e.target.classList.contains('btn--show-modal')
+  ) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
